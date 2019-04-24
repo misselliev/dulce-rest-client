39862378f40e1d213b6ec0062260c2e26f8c3700
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-04-24 14:02:40 -0700

    complete rest-client

---
+++ dulce.rb
@@ -0,0 +1,12 @@
+require 'rest-client'
+
+puts "Welcome to Bing!"
+puts "Type your search: "
+search = gets.chomp.split.join("+")
+
+response = RestClient.get("http://www.bing.com/search?q=#{search}")
+
+puts "HTTP response code is: \n#{response.code}"
+puts "The hash of HTTP response headers is: \n#{response.headers}"
+puts "The hash of HTTP response cookies is: \n#{response.cookies}"
+puts "The response body is: \n#{response.body}"
