c78124b6546b6810746595bdd60be93b917bcfac
Santiago Torres <thdealer@gmail.com>
2019-04-26 13:24:44 -0500

    adding a search

--- dulce.rb
+++ dulce.rb
@@ -10,3 +10,12 @@ puts "HTTP response code is: \n#{response.code}"
 puts "The hash of HTTP response headers is: \n#{response.headers}"
 puts "The hash of HTTP response cookies is: \n#{response.cookies}"
 puts "The response body is: \n#{response.body}"
+
+
+new_search = "42+vida+universo"
+response = RestClient.get("http://www.bing.com/search?q=#{new_search}")
+
+puts "HTTP response code is: \n#{response.code}"
+puts "The hash of HTTP response headers is: \n#{response.headers}"
+puts "The hash of HTTP response cookies is: \n#{response.cookies}"
+puts "The response body is: \n#{response.body}"
